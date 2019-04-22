cae5738ea71e579e15ba1d1c20c13351a0bd646c
anilmandapaka <anilkumar.mandapaka@gmail.com>
2019-04-22 16:08:05 +0530

    rm helloworld

--- helloworld.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-int main()
-{
-   // printf() displays the string inside quotation
-   printf("Hello, World!");
-   printf("anilkumar.mandapaka@gmail.com")
-   printf("ptgindia")
-   return 0;
-}
